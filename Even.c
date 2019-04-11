ab77b2b63b5b3b445e14723e6ae6e3202e07624d
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-04-11 12:20:14 +0530

    Create Even.c

---
+++ Even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("Enter an interger:");
+scanf("%d:,&number);
+if(number % 2==0)
+     printf("%d is even.", number);
+ else
+     printf("%d is odd.", number);
+ return 0;
+ }
