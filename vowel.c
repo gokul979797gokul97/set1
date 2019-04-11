328d42b270cf75b9b09c2c80b8bde439e4fa23e1
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-04-11 12:42:07 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+chat c;
+scanf("%c",&c);
+if(c == 'a' || c== 'e' || c== 'i' || c =='o' || c=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("consonent");
+}
+return 0;
+}
