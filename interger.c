4176cb1ebe15ccf57be917ca52934a4c5975a5b0
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-02 06:56:52 -0700

    integer.c

---
+++ interger.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i;
+int sum=0;
+printf("Enter the n and k value\n");
+scanf("%d",&n);
+scanf("%d",&k);
+int array[n];
+for (i=0;i<n;i++)
+{
+ scanf("%d",&array[i]);
+ }
+ for(i=0;i<k;i++)
+ {
+ sum=sum+array[i]);
+ }
+ printf("%d",sum);
+ return 0;
+ }
