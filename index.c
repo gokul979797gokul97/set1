8b61462102767ac05f61c8cfdb887a1055a62fd3
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-03 11:47:02 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int a[n],i;
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    printf("%d\t %d",i,a[i]);
+  }
+}
