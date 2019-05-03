07d36f947589c7515a20fe0aad56873c7efe9f69
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-03 12:21:23 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n,num1=1,num2,i;
+  printf("%d %d",num1,num2);
+  for(i=0;i<n;i++)
+  {
+    num3=num1+num2;
+    printf("%d",num3);
+    num1=num2;
+    num2=num3
+  }
+ return 0;
+}
