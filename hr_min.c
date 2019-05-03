7bb213d205e6d39d360ce955c2bed30b8823a7ec
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-03 11:55:20 +0530

    Create hr_min.c

---
+++ hr_min.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int hr1,hr2,hr3,min1,min2,min3;
+  scanf("%d %d",&hr1,&min1);
+  scanf("%d %d",&hr2,&min2);
+  hr3=hr1-hr2;
+  min3=min1-min2;
+  printff("%d %d",&hr3,&min3);
+  
+}
