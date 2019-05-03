0c983b49c6179885c99cd812ba0ef6c44031f0f5
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-03 11:22:03 +0530

    Create arr_min.c

---
+++ arr_min.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int a[n],i,largest;
+  for(i=0;i<n;i++)
+  {
+  	scanf("%d",&a[i]);
+  }
+  largest=a[0];
+  for(i=0;i<n;i++)
+  {
+    if(largest < a[i])
+    {
+      largest=a[i];
+    }
+  }
+  printf("%d",largest);
+  return 0;
+}
