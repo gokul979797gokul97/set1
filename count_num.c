2b17815bef85ea6391db79b27aa70623c0da7425
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-09 11:52:43 +0530

    count_num.c

---
+++ count_num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int digit,i=0, remainder;
+ int sum=0;
+ scanf("%d", &digit);
+ while(i<digit)
+ {
+ remainder=digit%10;
+digit=digit/10;
+i++;
+}
+printf("%d", i+1);
+return 0;
+}
