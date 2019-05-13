d93e87edba4aedd6d63631ac042a97bd81911d75
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-13 16:04:02 +0530

    word.c

---
+++ word.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ scanf("%d",&n);
+ if((n>=1) &&(n<=10))
+ {
+  if(n==1)
+  {
+   printf("one");
+   }
+ else if(n == 2)
+ {
+   printf("Two");
+   }
+    else if (n== 3)
+    {
+    printf("Three");
+    }
+    else if(n==4)
+{
+ printf("four")
+ }
+ else if(n==5)
+ {
+   printf("five");
+   }
+    else if ("six");
+    }
+     else if(n==7)
+     {
+        printf("seven");    
+    }
+    else if (n==8)
+    {
+    printf("Eight");
+    }
+    else if(n == 9)
+    {
+    printf("nine");
+    }
+    else
+    {
+printf("ten");
+}
+}
+return 0;
+}
+    
+    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
