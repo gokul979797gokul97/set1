851d768a063714505614f2779a791b45387c6c4f
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 20:13:09 +0530

    factor.c

---
+++ factor.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int N,i;
+  scanf("%d",&N);
+  if(N<=1000)
+  {
+    for(i=1;i<=n;i++)
+    {
+       if(N % i==0)
+       {
+          printf("%d\t",i);
+          }
+         }
+         }
+         return 0;
+         }
