634904b97663067eaf9dbd623bd403ad68b91a9a
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 20:37:14 +0530

    kabali.c

---
+++ kabali.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ int kabalis_clan,opponents_clan,difference;
+ scanf("%d %d", &kabalis_clan,&opponents_clan);
+ difference=opponent's_clan_kabalis_clan;
+ printf("%d",difference);
+ return 0;
+ }
