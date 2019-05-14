efac19f3c36609d429221d95a51f249e828e3ddd
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 19:17:43 +0530

    multiple_seven.c

---
+++ multiple_seven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int number;
+ scanf("%d", &number);
+ if(number %7==0)
+ {
+    printf("Yes");
+ }
+ else
+      {
+       printf("No");
+      }
+      return 0;
+      }
