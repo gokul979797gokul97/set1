f2e6ccc10c67d8ed739972900eebd27335edff46
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 19:22:02 +0530

    diff_odd_even.c

---
+++ diff_odd_even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{   
+ int num1,num2,diff;
+ scanf("%d %d", &num1,&num2);
+ diff=num1-num2;
+ if(diff% 2 ==0)
+ {
+    printf("even");
+ }
+  else
+  {
+    printf("No");
+  }
+   return 0;
+   }
+ 
