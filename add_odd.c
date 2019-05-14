9ebbce85a561dbd584a2250a55827502fe4d8bfd
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 12:09:25 +0530

    add_odd.c

---
+++ add_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int n,m,add;
+   scanf("%d %d",&n,&m);
+   add=n+m;
+   if(add % 2 ==0)
+   {
+     printf("Even");
+     }
+     else
+      {
+      printf("odd");
+      }
+      return 0;
+      }
