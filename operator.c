bc56d72430bc5b3b1c651d2df6c18580f791341d
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 20:48:32 +0530

    operator.c

---
+++ operator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int input1,input2,ans;
+ char op;
+ scanf("%d %c %d",&input1,&op,&op,&input2);
+ if(op == '/')
+ {
+  ans =input1/input2;
+  }
+   else if(op == '%')
+   {
+     ans=input1%input2;
+        }
+        printf("%d",ans);
+        return o;
+        }
