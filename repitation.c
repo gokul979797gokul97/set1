1ef899989faacda8dd53ab982634cb74193ac26b
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 11:18:13 +0530

    repitation.c

---
+++ repitation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int n,k,i,count=0;
+  scanf("%d %d" , &n,&k);
+  int aee[n];
+  for(i=0;i<n;i++)
+  {
+     scanf("&d",&arr[i]);
+  }
+   for(i=0;i<n;i++)
+   {
+      if(arr[i]==k)
+      {
+       count++
+       }
+       }
+       printf("%d",count);
+       return 0;
+       }
