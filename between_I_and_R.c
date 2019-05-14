fbcff4f9efd2d3cbb513a878d357f5939d2f6f87
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 19:50:06 +0530

    between_I_and_R.c

---
+++ between_I_and_R.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int N,L,R,i,count=0;
+  scanf("%d", &N);
+  scanf("%d %d", &L,&R);
+  for(i=L;I<R;i++)
+  {
+    if(N==i)
+    {
+      count =1;
+      }
+      }
+      if(count == 1)
+      {
+        printf("Yes");
+      }
+      else
+       {
+          printf("No");
+       }
+        return 0;
+        }
