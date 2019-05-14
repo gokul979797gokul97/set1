69c7e13e5a366f43ba9010a1683540b39a834093
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-05-14 11:45:58 +0530

    natural_sum.c

---
+++ natural_sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum;
+    printf("Enter the number");
+    scanf("%d",&n);
+    if(n>=1)
+    {
+       sum=0;
+       for(i=1;i<=n;i++)
+       {
+  sum=sum+1;
+  }
+      printf("d",sum);
+      }
+         return 0;
+         }
