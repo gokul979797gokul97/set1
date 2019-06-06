e16da332b43d2de75f4610ba28f425e73072f8a4
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-06-06 14:26:47 +0530

    kth_element.c

---
+++ kth_element.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int n,k,i;
+  scanf("%d %d", &N,&K);
+  int arr[N];
+  for(i=0;i<N;i++)
+  {
+   scanf("%d",&arr[i]);
+   }
+    printf("%d",arr[k-1]);
+    return 0;
+    }
