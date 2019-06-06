db5a072a6ccf20dcc82705ad3ca646f367d26aa1
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-06-06 13:52:24 +0530

    cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int 1,b,h,volume,surface;
+   scanf("%d %d %d",&1,&b,&h);
+   volume =1*b*h;
+    surface=(2*1*b)+(2*b*h)+(2*1*h);
+    printf("%d %d",surface,volume);
+    return 0;
+    }
