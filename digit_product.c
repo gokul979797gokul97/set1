a6d9d7f28218d210502807bd94a03805c50412f7
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-06-06 14:49:15 +0530

    digit_product.c

---
+++ digit_product.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int digit,remainder.product=1;
+ scanf("%d",&digit);
+ while(digit)
+ {
+  remaining=digit%10;
+  product=product*remainning;
+  digit=digit/10;
+  }
+   printf("%d",product);
+   return 0;
+   }
+ 
