057df9f395f6beb04b80a93428dfc7ab42906720
gokul979797gokul97 <49308106+gokul979797gokul97@users.noreply.github.com>
2019-06-06 13:27:03 +0530

    isogram.py

---
+++ isogram.py
@@ -0,0 +1,15 @@
+str=input()
+L=[]
+str1=''
+index=0
+ for index in str:
+    if index not in L:
+      L.append(index)
+      str1=''.join(L)
+      Length1=len(str)
+      Length1=len(str1)
+      if(Length1-Length2 ==0);
+      print("Yes")
+      else
+      print("No")
+      
